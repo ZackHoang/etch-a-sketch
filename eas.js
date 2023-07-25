03a459f955a53bacbe045da951bb22703d0c062f
Zack <zackhoang2004@gmail.com>
2023-07-25 22:38:45 +0700

    Add button for new grid

--- eas.js
+++ eas.js
@@ -1,6 +1,6 @@
-const container = document.getElementById("container");
+const container = document.getElementById("container");  
 
-function makeRows(rows, cols) {
+function makeGrid(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
@@ -9,7 +9,7 @@ function makeRows(rows, cols) {
   };
 };
 
-makeRows(16, 16);
+makeGrid(16, 16);
 
 const divs = document.getElementsByClassName('grid-item');
 for (let i = 0; i < divs.length; i++) {
@@ -17,6 +17,26 @@ for (let i = 0; i < divs.length; i++) {
     e.target.style.backgroundColor = 'black';
     console.log(e.target); 
   }); 
-}   
+}
+
+function createNewGrid() {
+  let userInput = prompt('Enter a number to change your grid size: ');
+  console.log(Number(userInput));
+  if (isNaN(userInput)) {
+    alert ('Invalid input. Please enter a number.');
+    console.log('is not a number');  
+  } else if (userInput > 100) {
+    alert ('This value is too high. Please enter a lower value'); 
+  } else {
+    container.innerHTML = "";
+    makeGrid(userInput, userInput);
+    const divs = document.getElementsByClassName('grid-item');
+    for (let i = 0; i < divs.length; i++) {
+      divs[i].addEventListener('mouseover', function(e) {
+      e.target.style.backgroundColor = 'black';
+      });
+    }
+  }
+}  
     
 
